c404ab53b5a6cc134bc6f0257169856d7c3aa25d
Harshgg <gurnasinghaniharsh@gmail.com>
2024-04-24 17:55:57 +0530

    add carousel for places

--- Chhabria Housing/Chhabria Housing/src/Listofplaces.jsx
+++ Chhabria Housing/Chhabria Housing/src/Listofplaces.jsx
@@ -1,31 +1,40 @@
 import React from "react";
 import CSS from "./Listofplaces.module.css";
+import Marquee from "react-fast-marquee";
 
 export default function AutoplayCarousel() {
 
   return (
-    <div className={CSS.Outer}>
-      <h1 className={CSS.head}>Places Of Operation</h1>
-      <div className={CSS.carouselcontainer}>
-        <div className={CSS.carouseltrack}>
-          <h1 className={CSS.carouselitem}>Juhu</h1>
-            <h1 className={CSS.carouselitem}>Church Gate</h1>
-            <h1 className={CSS.carouselitem}>Dadar </h1>
-            <h1 className={CSS.carouselitem}>Bandra </h1>
-            <h1 className={CSS.carouselitem}>Andheri </h1>
-            <h1 className={CSS.carouselitem}>Goregaon </h1>
-            <h1 className={CSS.carouselitem}>Malad </h1>
-            <h1 className={CSS.carouselitem}>Kandivali </h1>
-            <h1 className={CSS.carouselitem}>Borivali </h1>
-            <h1 className={CSS.carouselitem}>Dahisar </h1>
-            <h1 className={CSS.carouselitem}>Mira Road </h1>
-            <h1 className={CSS.carouselitem}>Bhayander </h1>
-            <h1 className={CSS.carouselitem}>Chembur </h1>
-            <h1 className={CSS.carouselitem}>Kanjurmarg </h1>
-            <h1 className={CSS.carouselitem}>Mulund </h1>
-
-        </div>
-      </div>
-    </div>
+    <>
+    <h1 className={CSS.head}>Our Places of Operation</h1>
+    <Marquee speed={110}>
+            <h1 className={CSS.carouselitem}><span className={CSS.special}>Dubai ⠀⠀⠀</span></h1>
+            <h1 className={CSS.carouselitem}><span className={CSS.special}>London ⠀⠀⠀</span></h1>
+            <h1 className={CSS.carouselitem}>Nariman Point ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Marine Lines ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Santacruz ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Khar ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Ghatkoper ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Powai ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Chembur ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Walkeshwar ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Breach Candy ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Nepeansea Road ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Altamount Road ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Carmichael Road ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Worli ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Juhu ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Tardeo ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Mahalakshmi ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Lower Parel ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Prabhadevi ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Dadar ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>Parel ⠀⠀⠀</h1>
+            <h1 className={CSS.carouselitem}>BKC ⠀⠀⠀</h1>
+  </Marquee>
+  <br></br>
+  <br></br>
+  <br></br>
+  </>
   );
 }
--- Chhabria Housing/Chhabria Housing/src/Testimonial.jsx
+++ Chhabria Housing/Chhabria Housing/src/Testimonial.jsx
@@ -23,41 +23,40 @@ function Testimonial() {
         <div ref={testimonialRef} className={`${testicss.outer} ${isVisible && testicss.visible}`}>
             <div className={testicss.testi}>
                 <h1 className={testicss.testimonial}>What Our Clients Say About Us</h1>
-                <h2 className={testicss.head}>Read firsthand experiences and testimonials from our valued clients</h2>
                 <div className={testicss.testimonialcontain}>
                     <div className={`${testicss.testimonialbox} ${isVisible && testicss.fadeIn}`}>
-                        <h3 className={testicss.name}>Rohan Shah</h3>
+                        <h3 className={testicss.name}>Kumar Shah</h3>
                         <p className={testicss.mess}>
                             Living in Mumbai's fast-paced environment, I needed a service
                             provider that matches the city's energy. These guys surpassed my
                             expectations. Kudos
                         </p>
                         <p>
-                            <strong>Mumbai Central</strong>
+                            <strong>Juhu</strong>
                         </p>
                     </div>
 
                     <div className={`${testicss.testimonialbox} ${isVisible && testicss.fadeIn}`}>
-                        <h3 className={testicss.name}>Akash Sharma</h3>
+                        <h3 className={testicss.name}>Dilip Verma</h3>
                         <p className={testicss.mess}>
                             Reliability is key in Mumbai, and this team embodies it. Their
                             attention to detail and customer-centric approach make them stand
                             out. Great job!
                         </p>
                         <p>
-                            <strong>Kanjur Marg</strong>
+                            <strong>Lower Parel</strong>
                         </p>
                     </div>
 
                     <div className={`${testicss.testimonialbox} ${isVisible && testicss.fadeIn}`}>
-                        <h3 className={testicss.name}>Ayesha Khan</h3>
+                        <h3 className={testicss.name}>Tushar Gadge</h3>
                         <p className={testicss.mess}>
                             Living in the bustling city of Mumbai, finding trustworthy services
                             can be challenging. This team not only meets expectations but
                             exceeds them. Truly satisfied!
                         </p>
                         <p>
-                            <strong>Borivali</strong>
+                            <strong>BKC</strong>
                         </p>
                     </div>
                 </div>
